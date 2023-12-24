7f3e81ec6289e11eef61762587c0d6e545c37b16
Samuel Souza <ssouza.himself@gmail.com>
2023-12-24 16:24:37 -0300

    patterns to get Class Object

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ Fundamental classes:
 - Method
 - Constructor
 - Annotation
+
+Three Patterns to get a Class Instance
+
+- from an Object
+
+```java
+String hello = "Hello";
+Class helloClass = hello.getClass();
+```
+
+- from a known class
+
+```java
+Class helloClassInstance = String.class;
+```
+
+- from the name of the class
+
+```java
+String className = "java.lang.String";
+Class helloClass = Class.forName(className);
+```
+
+    
--- java-reflection-api/src/Main.java
+++ java-reflection-api/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         String hello = "Hello";
         Class helloClass = hello.getClass();
 
@@ -9,5 +9,24 @@ public class Main {
         Class<?> helloClass2 = "World".getClass();
 
         // Class<String> helloClass3 = "error".getClass();
+        // Class<Object> helloClass4 = "error".getClass();
+
+        Class<? extends String> helloClass5 = "works".getClass();
+        Class<? extends Object> helloClass6 = "works".getClass();
+
+        Class<?> helloClassInstance = String.class;
+
+        System.out.println(helloClassInstance.getSimpleName());
+
+        String className = helloClassInstance.getName();
+        Class<?> helloClass7 = Class.forName(className);
+
+        System.out.println(helloClass7.getName());
+
+        try{
+            Class<?> unknownClass = Class.forName("DoesNotExists");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
