0d740492f5da0207c011000169ab906c4cd4aacf
Samuel Souza <ssouza.himself@gmail.com>
2023-12-26 23:00:30 -0300

    define retention policy

--- java-metamodels/src/dev/ssouza/metamodels/annotation/PrimaryKey.java
+++ java-metamodels/src/dev/ssouza/metamodels/annotation/PrimaryKey.java
@@ -1,5 +1,9 @@
 package dev.ssouza.metamodels.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
     
 }
