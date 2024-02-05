e01136552a02a57e0f192099d74d7ea2b82fd5db
Samuel Souza <ssouza.himself@gmail.com>
2024-02-05 20:47:02 -0300

    using overload to print array

---
+++ java-generics/src/main/java/dev/ssouza/generics/Main.java
@@ -0,0 +1,37 @@
+package dev.ssouza.generics;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] numbers = { 1, 2, 3, 4, 5 };
+        Double[] doubles = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        Character[] chars = { 'A', 'E', 'I', 'O', 'U' };
+        String[] strings = { "Hello", "World" };
+
+        printArray(numbers);
+        printArray(doubles);
+        printArray(chars);
+        printArray(strings);
+    }
+
+    public static void printArray(Integer[] numbers) {
+        Arrays.stream(numbers)
+                .forEach(System.out::println);
+    }
+
+    public static void printArray(Double[] doubles) {
+        Arrays.stream(doubles)
+                .forEach(System.out::println);
+    }
+
+    public static void printArray(Character[] chars) {
+        Arrays.stream(chars)
+                .forEach(System.out::println);
+    }
+
+    public static void printArray(String[] strings) {
+        Arrays.stream(strings)
+                .forEach(System.out::println);
+    }
+}
