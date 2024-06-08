d2f365314993aa43038eaa3a2362c7da6c10fcae
Samuel <ssouza@that.dev>
2024-06-08 14:03:38 -0300

    chapter 7

---
+++ tdd-with-java/src/main/java/dev/ssouza/Money.java
@@ -0,0 +1,11 @@
+package dev.ssouza;
+
+public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+}
