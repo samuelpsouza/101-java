5341ef580ee491193f280a7a22262f9b9182d711
Samuel <ssouza@that.dev>
2024-06-08 12:46:26 -0300

    chapter 1

---
+++ tdd-with-java/src/main/java/dev/ssouza/Dollar.java
@@ -0,0 +1,12 @@
+package dev.ssouza;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier){
+        amount *= multiplier;
+    }
+}
